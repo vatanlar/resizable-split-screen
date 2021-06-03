f4eb71af26d4a8a07b711b362dda7f3c880a88b3
Mehmet Ali VATANLAR <33216260+vatanlar@users.noreply.github.com>
2021-06-03 13:30:18 +0300

    Add files via upload

---
+++ assets/sceleton.js
@@ -0,0 +1,130 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = document.cookie;
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const resizable = function(resizer) {
+        const direction = resizer.getAttribute('data-direction') || 'horizontal';
+        const prevSibling = resizer.previousElementSibling;
+        const nextSibling = resizer.nextElementSibling;
+
+        // The current position of mouse
+        let x = 0;
+        let y = 0;
+        let prevSiblingHeight = 0;
+        let prevSiblingWidth = 0;
+
+        // Handle the mousedown event
+        // that's triggered when user drags the resizer
+        const mouseDownHandler = function(e) {
+            // Get the current mouse position
+            x = e.clientX;
+            y = e.clientY;
+            const rect = prevSibling.getBoundingClientRect();
+            prevSiblingHeight = rect.height;
+            prevSiblingWidth = rect.width;
+
+            // Attach the listeners to `document`
+            document.addEventListener('mousemove', mouseMoveHandler);
+            document.addEventListener('mouseup', mouseUpHandler);
+        };
+
+        const mouseMoveHandler = function(e) {
+            // How far the mouse has been moved
+            const dx = e.clientX - x;
+            const dy = e.clientY - y;
+
+            switch (direction) {
+                case 'vertical':
+                    const h = (prevSiblingHeight + dy) * 100 / resizer.parentNode.getBoundingClientRect().height;
+                    prevSibling.style.height = `${h}%`;
+                    setCookie("vertical", `${h}%`, 365);
+                    break;
+                case 'horizontal':
+                default:
+                    const w = (prevSiblingWidth + dx) * 100 / resizer.parentNode.getBoundingClientRect().width;
+                    prevSibling.style.width = `${w}%`;
+                    setCookie("horizontal", `${w}%`, 365);
+                    console.log(w);
+                    break;
+            }
+
+            const cursor = direction === 'horizontal' ? 'col-resize' : 'row-resize';
+            resizer.style.cursor = cursor;
+            document.body.style.cursor = cursor;
+
+            prevSibling.style.userSelect = 'none';
+            prevSibling.style.pointerEvents = 'none';
+
+            nextSibling.style.userSelect = 'none';
+            nextSibling.style.pointerEvents = 'none';
+        };
+
+        const mouseUpHandler = function() {
+            resizer.style.removeProperty('cursor');
+            document.body.style.removeProperty('cursor');
+
+            prevSibling.style.removeProperty('user-select');
+            prevSibling.style.removeProperty('pointer-events');
+
+            nextSibling.style.removeProperty('user-select');
+            nextSibling.style.removeProperty('pointer-events');
+
+            // Remove the handlers of `mousemove` and `mouseup`
+            document.removeEventListener('mousemove', mouseMoveHandler);
+            document.removeEventListener('mouseup', mouseUpHandler);
+        };
+
+        // Attach the handler
+        resizer.addEventListener('mousedown', mouseDownHandler);
+    };
+
+    // Query all resizers
+    document.querySelectorAll('.resizer').forEach(function(ele) {
+        resizable(ele);
+    });
+});
+
+
+$( document ).ready(function() {
+    
+
+    var cookieHeight = getCookie("vertical");
+    var cookieWidth = getCookie("horizontal");
+
+    if(cookieHeight!=""){
+        $(".container__top").animate({
+            height: cookieHeight
+        }, 500);    
+    }
+
+    if(cookieWidth!=""){
+        $(".container__left").animate({
+            width: cookieWidth
+        }, 500);
+    }
+
+
+});
+
+
